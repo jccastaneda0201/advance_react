e5226140ba5eebab4543943f8e85169aa6dfbfd1
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-06-27 12:16:13 +0200

    update commit

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -11,12 +11,33 @@ const MultipleReturnsFetchData = () => {
       try {
         const resp = await fetch(url);
         const user = await resp.json();
-        console.log(user);
+        setUser(user);
       } catch (error) {
         console.log(error);
       }
+      setIsLoading(false);
     };
     fetchUser();
   }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error...</h2>;
+  }
+  return (
+    <div>
+      <img
+        style={{ width: '150px', borderRadius: '25px' }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <h4>works at {user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
